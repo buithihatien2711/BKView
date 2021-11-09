72c1bbeb8c133f9b80ae7e2c28b1f18fa8e2b99f
buithihatien2711 <buithihatien2711@gmail.com>
2021-11-09 20:51:12 +0700

    09/11/2021

--- src/Client/ReceiveScreen.java
+++ src/Client/ReceiveScreen.java
@@ -1,9 +1,13 @@
 package Client;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.zip.GZIPInputStream;
 import java.awt.Image;
 import java.awt.Graphics;
 import javax.imageio.ImageIO;
@@ -27,6 +31,7 @@ public class ReceiveScreen extends Thread{
 	public void run() {
 		try {
 			while (true) {
+				//Receive compressed image
 				byte[] bytes = new byte[1024*1024];
 				int count = 0;
 				count += inputStream.read(bytes, count, bytes.length - count);
@@ -36,7 +41,43 @@ public class ReceiveScreen extends Thread{
 					count += inputStream.read(bytes, count, bytes.length - count);
 				} while (!(count > 4  && bytes[count-2]==(byte)-1 && bytes[count-1]==(byte)-39 ));
 				
-				image = ImageIO.read(new ByteArrayInputStream(bytes));
+				//Convert byte to InputStream
+				//is is stream of input stream of gzip file
+				InputStream is = new ByteArrayInputStream(bytes);
+				
+				//Decompress image
+				GZIPInputStream gzipIS = new GZIPInputStream(is);
+				byte[] buffer = new byte[1024*1024];
+				int len = gzipIS.read(buffer);
+				
+				//Doc tu gzip vao buffer roi doc tu buffer vao file output stream
+				OutputStream os;
+//				while ((len = gzipIS.read(buffer)) != -1) {
+//					os.write(buffer, 0, len);
+//				}
+				
+				while (len != -1) {
+					len = gzipIS.read(buffer);
+				}
+				
+				
+				//image read from input stream
+				InputStream instream = new ByteArrayInputStream(buffer);
+				image = ImageIO.read(instream);
+				
+				
+				
+//				byte[] bytes = new byte[1024*1024];
+//				int count = 0;
+//				count += inputStream.read(bytes, count, bytes.length - count);
+//				
+//				//Ket thuc cua luong hinh anh la 0xFFD9 
+//				do {
+//					count += inputStream.read(bytes, count, bytes.length - count);
+//				} while (!(count > 4  && bytes[count-2]==(byte)-1 && bytes[count-1]==(byte)-39 ));
+//				
+//				image = ImageIO.read(new ByteArrayInputStream(bytes));
+				
 				image = image.getScaledInstance(jPanel.getWidth(), jPanel.getHeight(), Image.SCALE_FAST);
 				
 				Graphics graphics = jPanel.getGraphics();
--- src/Server/SendScreen.java
+++ src/Server/SendScreen.java
@@ -2,9 +2,16 @@ package Server;
 
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.zip.GZIPOutputStream;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
@@ -25,14 +32,64 @@ public class SendScreen extends Thread{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+//		try {
+//			outputStream = socket.getOutputStream();
+//			while (true) {
+//				BufferedImage image =  robot.createScreenCapture(rectangle);
+//				ImageIO.write(image, "jpeg", outputStream);
+//				Thread.sleep(10);
+//			}
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
 		try {
 			outputStream = socket.getOutputStream();
-			while (true) {
-				BufferedImage image =  robot.createScreenCapture(rectangle);
-				ImageIO.write(image, "jpeg", outputStream);
-				Thread.sleep(10);
+			FileInputStream fis = null;
+			FileOutputStream fos = null;
+			GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+			
+			//Take a screenshot
+			BufferedImage bufferedImage =  robot.createScreenCapture(rectangle);
+			
+			//Convert a BufferImage to a byte array
+			ByteArrayOutputStream boas = new ByteArrayOutputStream();
+			ImageIO.write(bufferedImage, "jpg", boas);
+			byte[] bytes = boas.toByteArray();
+			
+			//Convert a BufferedImage to InputStream
+			InputStream is = new ByteArrayInputStream(boas.toByteArray());
+			
+			
+			//Compress image
+			byte[] buffer = new byte[1024*1024];
+			int len;
+			while ((len = is.read(buffer)) != -1) {
+				gzipOS.write(buffer, 0, len);
 			}
-		} catch (Exception e) {
+			
+			//Send byte array
+			outputStream.write(buffer);
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+//			byte[] buffer = new byte[1024];
+//			
+//			int bytes_read;
+//			int len;
+//            
+//            while ((len = fis.read(buffer)) != -1) {
+//                gzip.write(buffer, 0, bytes_read);
+//            }
+            
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
